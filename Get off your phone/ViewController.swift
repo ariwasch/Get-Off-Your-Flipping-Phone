f95b16b35f55e8447367e37eac9e080cefe2e2eb
Ari Wasch <ariwasch@gmail.com>
2019-12-31 13:34:26 -0500

    Initial Commit

---
+++ Get off your phone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get off your phone
+//
+//  Created by Ari Wasch on 12/31/19.
+//  Copyright © 2019 Ari Wasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
